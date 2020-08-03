074d320a47a6235e7b38f160239c67869e4bcd41
Luis Gomez <49451508+LuiDEVBubble@users.noreply.github.com>
2020-08-03 12:27:36 -0400

    Number()

--- modules/stringToInt.js
+++ modules/stringToInt.js
@@ -1,12 +1,29 @@
-let profit = "200" - "150";
+// let profit = "200" - "150";
 
-console.log(profit);
+// console.log(profit);
 
-let resultConcat = "200" + 150;
+// let resultConcat = "200" + 150;
 
 
-console.log(resultConcat);
+// console.log(resultConcat);
 
-let myFractional = parseFloat("1.999"); // If you want the converted string to keep the decimal places then use parsefloat() instead of parseInt().
+// let myFractional = parseFloat("1.999"); // If you want the converted string to keep the decimal places then use parsefloat() instead of parseInt().
 
-console.log(myFractional + 10);
+// console.log(myFractional + 10);
+
+
+//========================================
+
+//The following code uses the function Number() in order to convert a string with a float or whole number into what they are
+
+
+// let intNum = "24";
+
+// let floatNum = "24.35352352";
+
+// intNum = Number(intNum);
+
+// floatNum = Number(floatNum);
+
+// //I put the above variables in () in order to see if they've actually been converted to numbers.
+// console.log("The whole number is " + (intNum + 5) + " And the float number is " + (floatNum + 10) );
